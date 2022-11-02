fe87a705d5b0c6f36cc0bf693e51f88daef048ca
Shubh Arya <shubh.arya37@gmail.com>
2022-11-03 00:17:13 +0530

    created the view and added logic (#1)
    * Update 2 files
    
    * incremental like-me button
    
    * rendering input in the view using useState
    
    * created emoji database and showed its meaning in view
    
    * looped over an array using map function
    
    * added both input and click handler and tested

--- src/App.js
+++ src/App.js
@@ -1,10 +1,49 @@
+import React, { useState } from 'react';
 import "./styles.css";
 
 export default function App() {
+  const emojiDict = {
+    "🤗":"hugging",
+    "🤩": "over-excited",
+    "🥱": "yawning",
+    "😫": "disgusted",
+    "😛": "teasing",
+    "🤤": "mouth-watering",
+    "😲": "shocked",
+    "🤑": "money-time"
+  }
+ 
+ var emojiArr = Object.keys(emojiDict); 
+  
+  var [meaning, setMeaning] = useState("");
+  function inputHandler(event){
+     var input = event.target.value;
+     meaning = emojiDict[input];
+     if(meaning === undefined){
+       meaning = "we don't have this in our database";
+     }
+     setMeaning(meaning);
+  }
+  function clickHandler(item){
+    meaning = emojiDict[item];
+    setMeaning(meaning);
+  }
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <h1>Emoji Interpreter</h1>
+      <input onChange = {inputHandler}></input>
+      <div>{meaning}</div>
+      <h3> Emoji's we Know</h3>
+      <div>
+        {
+          emojiArr.map((item)=>{
+            return(
+            <span key= {item} style= {{padding: "0.5rem", fontSize: "1.5rem", cursor: "pointer"}} onClick = {()=>clickHandler(item)}>{item}</span>
+            );
+            
+          })
+        }
+      </div>
     </div>
   );
 }
