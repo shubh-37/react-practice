d1be03f18f6238c5a12f0d8f10bda7a594dc1dc5
Shubh Arya <shubh.arya37@gmail.com>
2022-11-02 05:37:41 +0000

    created emoji database and showed its meaning in view

--- src/App.js
+++ src/App.js
@@ -2,16 +2,27 @@ import React, { useState } from 'react';
 import "./styles.css";
 
 export default function App() {
-  var [input, setInput] = useState("");
+  const emojiDict = {
+    "🤗":"hugging",
+    "🤩": "over-excited",
+    "🥱": "yawning",
+    "😫": "disgusted",
+    "😛": "teasing",
+    "🤤": "drooling",
+    "😲": "shocked",
+    "🤑": "money-time"
+  }
+  var [meaning, setMeaning] = useState("");
   function inputHandler(event){
-     input = event.target.value;
-     setInput(input);
+     var input = event.target.value;
+     meaning = emojiDict[input];
+     setMeaning(meaning);
   }
   return (
     <div className="App">
       <h1>Emoji Interpreter</h1>
       <input onChange = {inputHandler}></input>
-      <div>{input}</div>
+      <div>{meaning}</div>
     </div>
   );
 }
