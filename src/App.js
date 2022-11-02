464f68740031adbe5106f347c1e1be60b374eff5
Shubh Arya <shubh.arya37@gmail.com>
2022-11-02 05:17:44 +0000

    incremental like-me button

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
+import React, { useState } from 'react';
 import "./styles.css";
 
 export default function App() {
+  var [likeCounter, setLikeMe] = useState(0);
+  function clickHandler(){
+    likeCounter = likeCounter + 1;
+    setLikeMe(likeCounter);
+  }
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <h1>Emoji Interpreter</h1>
+      <button onClick={clickHandler}>Like Me</button>{likeCounter}
     </div>
   );
 }
